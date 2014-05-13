441948b4ad15e3f70eeacbbf3ce191c3af38d503
Brett Morris <brettmorris21@gmail.com>
2014-05-13 12:03:04 -0700

    tuned planet sizes to 15, 1 R_earth

--- plots.py
+++ plots.py
@@ -17,10 +17,12 @@ def faketransit(times, midtransit, duration, depth):
 times = np.linspace(0,1,100)    # times of fake photometric measurements
 names = ['b', 'c']              # name for each planet
 midtransits = [0.5, 0.5]        # time of mid-transit
-durations = [0.4, 0.7]          # duration of the transit event
-depths = [0.02, 0.005]          # fractional change in flux in transit event
-noise = 0.1*np.min(depths)       # simulated noise factor
+durations = [0.3, 0.6]          # duration of the transit event
+depths = [0.02, 0.00015]        # fractional change in flux in transit event
+noises = 0.1*np.array(depths)   # simulated noise factor
+answerkeyname = 'answerkey.txt'
 
+outfile = open(answerkeyname,'w')
 fig, ax = plt.subplots(1,len(depths),figsize=(14,6))
 
 for i in range(len(depths)):
@@ -28,7 +30,13 @@ for i in range(len(depths)):
     lightcurve = faketransit(times, midtransits[i], durations[i], depths[i])
     
     # Add gaussian noise to make the model look like data
-    lightcurve += noise*np.random.randn(len(lightcurve))
+    lightcurve += noises[i]*np.random.randn(len(lightcurve))
+    
+    # Assuming R_* = R_sun, and R_sun = 109*R_earth, 
+    R_planet = 109*np.sqrt(depths[i])
+    newline = 'Radius of planet %s = %f Earth radii\n' % (names[i], R_planet)
+    print newline
+    outfile.write(newline)
     
     ax[i].plot(times, lightcurve, 'k-o')
     ax[i].set_title('Planet %s' % names[i])
@@ -36,6 +44,8 @@ for i in range(len(depths)):
     ax[i].set_ylabel('Apparent Host-Star Brightness')
     ax[i].set_xlim([np.min(times), np.max(times)])
     ax[i].set_ylim([1-2*depths[i], 1+depths[i]])
+    ax[i].get_yaxis().get_major_formatter().set_useOffset(False)
     ax[i].grid()
 fig.savefig('plots/faketransit.pdf',bbox_inches='tight')
+outfile.close()
 plt.show()
