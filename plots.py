cb583eefd8ce3e0281fce10caec2ec7ed7ae2c66
Brett Morris <brettmorris21@gmail.com>
2014-05-13 11:47:09 -0700

    adding questions

--- plots.py
+++ plots.py
@@ -15,22 +15,27 @@ def faketransit(times, midtransit, duration, depth):
     return fluxes
     
 times = np.linspace(0,1,100)    # times of fake photometric measurements
-midtransit = 0.5                # time of mid-transit
-duration = 0.4                  # duration of the transit event
-depth = 0.01                    # fractional change in flux in transit event
-noise = 0.1*depth
+names = ['b', 'c']              # name for each planet
+midtransits = [0.5, 0.5]        # time of mid-transit
+durations = [0.4, 0.7]          # duration of the transit event
+depths = [0.02, 0.005]          # fractional change in flux in transit event
+noise = 0.1*np.min(depths)       # simulated noise factor
 
-# Generate (fake) model light curve
-lightcurve = faketransit(times, midtransit, duration, depth)
+fig, ax = plt.subplots(1,len(depths),figsize=(14,6))
 
-# Add gaussian noise to make the model look like data
-lightcurve += noise*np.random.randn(len(lightcurve))
-
-fig, ax = plt.subplots(1,1)
-ax.plot(times, lightcurve, 'k-o')
-ax.set_xlabel('Time (days)')
-ax.set_ylabel('Apparent Host-Star Brightness')
-ax.set_xlim([np.min(times), np.max(times)])
-ax.set_ylim([1-2*depth, 1+depth])
-fig.savefig('plots/faketransit.pdf')
+for i in range(len(depths)):
+    # Generate (fake) model light curve
+    lightcurve = faketransit(times, midtransits[i], durations[i], depths[i])
+    
+    # Add gaussian noise to make the model look like data
+    lightcurve += noise*np.random.randn(len(lightcurve))
+    
+    ax[i].plot(times, lightcurve, 'k-o')
+    ax[i].set_title('Planet %s' % names[i])
+    ax[i].set_xlabel('Time (days)')
+    ax[i].set_ylabel('Apparent Host-Star Brightness')
+    ax[i].set_xlim([np.min(times), np.max(times)])
+    ax[i].set_ylim([1-2*depths[i], 1+depths[i]])
+    ax[i].grid()
+fig.savefig('plots/faketransit.pdf',bbox_inches='tight')
 plt.show()
